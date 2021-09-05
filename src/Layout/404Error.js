3307dd3b13f840c18feecacd224fd4890823b608
buitrung3799 <buitrung19993@gmail.com>
2021-09-06 01:08:12 +0700

    Update 404Error.js

---
+++ src/Layout/404Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ErrorPage(props) {
+  return (
+    <div>
+      <div className="404-container"></div>
+      <div className="404-description">
+          
+      </div>
+    </div>
+  );
+}
